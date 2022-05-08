d91c07e0cd17768e0a302c4e40ce3969f20d8f43
Diogo Nunes <diogoffn@gmail.com>
2022-05-08 10:13:29 -0700

    created constants related to google sheets

--- src/lib/utils/constants.dart
+++ src/lib/utils/constants.dart
@@ -12,3 +12,23 @@ const navFoodFeup = 'Food FEUP';
 const faculties = ['faup', 'fbaup', 'fcup', 'fcnaup', 'fadeup',
                   'fdup', 'fep', 'feup', 'ffup', 'flup', 'fmup',
                   'fmdup', 'fpceup', 'icbas'];
+
+
+//google sheets related
+const _credentials = r'''
+{
+  "type": "service_account",
+  "project_id": "foodfeup123",
+  "private_key_id": "",
+  "private_key": "",
+  "client_email": "",
+  "client_id": "",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": ""
+}
+''';
+
+// your spreadsheet id
+const _spreadsheetId = '1DKzseOcDlRFbqeitILN_ygX6zxvPXkwXI6Zi8Llc3mM';
