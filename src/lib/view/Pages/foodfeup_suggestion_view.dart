710bf052bf4a087438acf3a55aee85ac90223a09
Diogo Nunes <diogoffn@gmail.com>
2022-05-15 10:12:02 -0700

    finished styling and spacing UI elements

--- src/lib/view/Pages/foodfeup_suggestion_view.dart
+++ src/lib/view/Pages/foodfeup_suggestion_view.dart
@@ -36,30 +36,43 @@ class FoodFeupSuggestionPageView extends StatelessWidget {
     return (SingleChildScrollView(
       scrollDirection: Axis.vertical,
       child: Column(
-        children: [createDropdownButton(),
-        PageTitle(name: establishment),
-        Text(mealType),
-        Row( children: [
-          Text("Stars go here"),
-          Text("(" + mealRatingQuant + ")")
-          ],
-          mainAxisAlignment: MainAxisAlignment.center,
-        ),
-        Text(mealName),
-        createReviewButton()]
+        children: [
+          Padding(padding: EdgeInsets.symmetric(horizontal: 0, vertical: 15)),
+          createDropdownButton(Theme.of(context).accentColor),
+          Padding(padding: EdgeInsets.symmetric(horizontal: 0, vertical: 15)),
+          Text(establishment,
+              style: TextStyle(fontWeight: FontWeight.w400,fontSize: 30),),
+          Padding(padding: EdgeInsets.symmetric(horizontal: 0, vertical: 15)),
+          Text(mealType,style: TextStyle(fontWeight: FontWeight.w500,fontSize: 30,
+              color:Theme.of(context).accentColor ),),
+          Padding(padding: EdgeInsets.symmetric(horizontal: 0, vertical: 3)),
+          Row( children: [
+            Text("Stars go here"),
+            Text("(" + mealRatingQuant + ")",style: TextStyle(fontWeight: FontWeight.w600,fontSize: 14,
+                color:Theme.of(context).accentColor ))
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+          Padding(padding: EdgeInsets.symmetric(horizontal: 0, vertical: 15)),
+          Divider(thickness: 3,indent: 20,endIndent: 20,color: Theme.of(context).accentColor),
+          Padding(padding: EdgeInsets.symmetric(horizontal: 0, vertical: 15)),
+          Text(mealName,style: TextStyle(fontWeight: FontWeight.w500,fontSize: 18,
+              color:Colors.black , ), textAlign: TextAlign.center,overflow: TextOverflow.fade),
+          Padding(padding: EdgeInsets.symmetric(horizontal: 0, vertical: 40)),
+          createReviewButton(Theme.of(context).accentColor)]
         ,)
       ));
     }
 
-    Widget createDropdownButton(){
+    Widget createDropdownButton(Color color){
       return DropdownButton<String>(
         value: dropdownValue,
         icon: const Icon(Icons.arrow_downward),
-        elevation: 16,
-        style: const TextStyle(color: Colors.red),
+        style: const TextStyle(color: Colors.black),
+        borderRadius: BorderRadius.circular(1),
         underline: Container(
           height: 2,
-          color: Colors.redAccent,
+          color: color,
         ),
         onChanged: (String newValue){//TODO:Make this update the page content
           dropdownValue = newValue;
@@ -73,7 +86,7 @@ class FoodFeupSuggestionPageView extends StatelessWidget {
       );
     }
 
-  Widget createReviewButton(){
+  Widget createReviewButton(Color color){
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
       child: SizedBox(
@@ -87,7 +100,7 @@ class FoodFeupSuggestionPageView extends StatelessWidget {
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(55),
               ),
-              primary: Colors.red,
+              primary: color,
             ),
             onPressed: decoy,//TODO change to real function
 
