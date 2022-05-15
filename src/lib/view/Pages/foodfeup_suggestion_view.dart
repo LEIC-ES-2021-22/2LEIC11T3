b2dc70a3c8cfb3db3fd64240751923aa3f6cea23
Diogo Nunes <diogoffn@gmail.com>
2022-05-15 07:07:53 -0700

    added all elements to suggestion page

--- src/lib/view/Pages/foodfeup_suggestion_view.dart
+++ src/lib/view/Pages/foodfeup_suggestion_view.dart
@@ -1,41 +1,103 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:uni/view/Widgets/page_title.dart';
 
 class FoodFeupSuggestionPageView extends StatelessWidget {
   FoodFeupSuggestionPageView({
     Key key,
-    @required this.options
+    @required this.options,
+    this.mealType,
+    this.mealRating,
+    this.mealRatingQuant,
+    this.mealName,
+    this.establishment
     });
 
   final List<String> options;
   String dropdownValue;
+  String mealType;
+  int mealRating;
+  String mealRatingQuant;
+  String mealName;
+  String establishment;
 
   @override
   Widget build(BuildContext context){
-  dropdownValue = options[0];
+    dropdownValue = options[0];//TODO: read these from somewhere
+    establishment = "Cantina almoço";
+    mealType = "Vegetariano";
+    mealRating = 4;
+    mealRatingQuant = "23";
+    mealName = "Jardineira de soja (batata,ervilhas e cenoura)";
 
-    return Column(
-      children: [
-        DropdownButton<String>(
-          value: dropdownValue,
-          icon: const Icon(Icons.arrow_downward),
-          elevation: 16,
-          style: const TextStyle(color: Colors.red),
-          underline: Container(
-            height: 2,
-            color: Colors.redAccent,
-          ),
-          onChanged: (String newValue){
-            dropdownValue = newValue;
-          },
-          items: options.map<DropdownMenuItem<String>>((String value) {
-            return DropdownMenuItem<String>(
+
+
+    return (SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Column(
+        children: [createDropdownButton(),
+        PageTitle(name: establishment),
+        Text(mealType),
+        Row( children: [
+          Text("Stars go here"),
+          Text("(" + mealRatingQuant + ")")
+          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+        Text(mealName),
+        createReviewButton()]
+        ,)
+      ));
+    }
+
+    Widget createDropdownButton(){
+      return DropdownButton<String>(
+        value: dropdownValue,
+        icon: const Icon(Icons.arrow_downward),
+        elevation: 16,
+        style: const TextStyle(color: Colors.red),
+        underline: Container(
+          height: 2,
+          color: Colors.redAccent,
+        ),
+        onChanged: (String newValue){//TODO:Make this update the page content
+          dropdownValue = newValue;
+        },
+        items: options.map<DropdownMenuItem<String>>((String value) {
+          return DropdownMenuItem<String>(
             value: value,
             child: Text(value),
-            );
-          }).toList(),
-        )],
+          );
+        }).toList(),
       );
     }
+
+  Widget createReviewButton(){
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+      child: SizedBox(
+          height: 55,
+          width: 55,
+          child: ElevatedButton(
+            child: Center(
+              child: Icon(Icons.star,color: Colors.white,size: 35,),
+            ),
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(55),
+              ),
+              primary: Colors.red,
+            ),
+            onPressed: decoy,//TODO change to real function
+
+          )
+      ),
+    );
+  }
+
+  bool decoy(){
+    return false;
+  }
+
   }
