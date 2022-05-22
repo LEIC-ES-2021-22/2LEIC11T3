c846a7a531800f2315add1d44329886a5b74c7e9
TiagoMRib <tiago18.2002@gmail.com>
2022-05-22 19:42:38 +0100

    Stars now double instead of int, toString method also added

--- src/lib/model/entities/review.dart
+++ src/lib/model/entities/review.dart
@@ -3,14 +3,14 @@ import 'package:uni/model/utils/day_of_week.dart';
 import 'package:uni/model/entities/meal.dart';
 
 class Review{
-  int stars;
+  double stars;
   Meal meal;
   int rest_id;
   String username;
   DateTime date;
   String comment;
 
-  Review(int st, String un, DateTime d, Meal m, int rid){
+  Review(double st, String un, DateTime d, Meal m, int rid){
     this.stars = st;
     this.username = un;
     this.date = d;
@@ -29,6 +29,21 @@ class Review{
   void printReview()
   {
     print("Stars: $stars , Username: $username , Comment: $comment ");
+
+  }
+
+  @override
+  String toString() {
+    // TODO: implement toString
+
+    if (comment != "")
+      {
+        return stars.toString() + "&" + username + "&" + comment;
+      }
+    else
+      {
+        return stars.toString() + "&" + username;
+      }
 
   }
 }
