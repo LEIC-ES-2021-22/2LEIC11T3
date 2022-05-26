7d9c00c6b5efae1f2f71862f896424ad555c9bba
mpspmf <mpspmf@gmail.com>
2022-05-26 23:44:39 +0100

    Removed padding from generateButtonText and centered some widgets

--- src/lib/view/Widgets/foodfeup_main_menu.dart
+++ src/lib/view/Widgets/foodfeup_main_menu.dart
@@ -67,8 +67,8 @@ class FoodFeupMainMenuState extends State<FoodFeupMainMenu>{
 
 bool transitionToEstablishment(BuildContext context, String buttonName, Map meals) {
   Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => FoodFeupEstablishmentPage(restaurantName: buttonName, meals: meals)));
+      context,
+      MaterialPageRoute(builder: (context) => FoodFeupEstablishmentPage(restaurantName: buttonName, meals: meals)));
 
   return true;
 }
@@ -85,10 +85,10 @@ Future<List<Widget>> getWidgets(BuildContext context) async {
   final List<Widget> widgets = [];
   await RestaurantFetcherHtml().getRestaurants(state).then((restaurants) {
     for(var rest in restaurants) {
-        widgets.add(Padding(padding: EdgeInsets.symmetric(horizontal: 8, vertical: 0)));
-        widgets.add(createButton(context, rest.name, rest.timetable, Colors.red, rest.meals, transitionToEstablishment));
-      }
+      widgets.add(Padding(padding: EdgeInsets.symmetric(horizontal: 8, vertical: 0)));
+      widgets.add(createButton(context, rest.name, rest.timetable, Colors.red, rest.meals, transitionToEstablishment));
     }
+  }
   );
 
   widgets.add(Padding(padding: EdgeInsets.symmetric(horizontal: 8, vertical: 0)));
@@ -102,33 +102,32 @@ Widget createButton(BuildContext context, String buttonName, String timeTable,
   return Padding(
     padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
     child: SizedBox(
-      height: 70,
-      width: 300,
-      child: ElevatedButton(
-        key: Key("establishment_button_$buttonName"),
-        style: ElevatedButton.styleFrom(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(25),
-          ),
-          primary: colour,
-        ),
-        onPressed: ()=> action(context, buttonName, meals),
+        height: 70,
+        width: 300,
+        child: ElevatedButton(
+            key: Key("establishment_button_$buttonName"),
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25),
+              ),
+              primary: colour,
+            ),
+            onPressed: ()=> action(context, buttonName, meals),
 
-        child: ListView(
-            children: generateButtonText(context, buttonName, timeTable),
-          ),
+            child: Center(
+              child: ListView(
+                shrinkWrap: true,
+                children: generateButtonText(context, buttonName, timeTable),
+              ),
+            )
         )
-      ),
+    ),
   );
 }
 
 List<Widget> generateButtonText(BuildContext context, String buttonName, String timeTable) {
   final List<Widget> widgets = [];
-  if(timeTable == '') {
-    widgets.add(Padding(padding: EdgeInsets.fromLTRB(0, 15, 0, 0)));
-  } else {
-    widgets.add(Padding(padding: EdgeInsets.fromLTRB(0, 10, 0, 0)));
-  }
+
 
   widgets.add(
     Center(
@@ -145,12 +144,13 @@ List<Widget> generateButtonText(BuildContext context, String buttonName, String 
       ),
     ),
   );
-  widgets.add(Text(timeTable,
-      style: TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.w400,
-          fontSize: 16),
-      textAlign: TextAlign.center));
-
+  if(timeTable != "") {
+    widgets.add(Text(timeTable,
+        style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w400,
+            fontSize: 16),
+        textAlign: TextAlign.center));
+  }
   return widgets;
 }
