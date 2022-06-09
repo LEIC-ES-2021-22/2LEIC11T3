fb4adcc9b1e6381c271c41191fc129d30eacd87f
TiagoMRib <tiago18.2002@gmail.com>
2022-06-09 10:48:58 +0100

    Create restaurant_page_view_test.dart

---
+++ src/test/unit/view/Pages/restaurant_page_view_test.dart
@@ -0,0 +1,6 @@
+import 'package:uni/model/entities/exam.dart';
+import 'package:uni/view/Pages/foodfeup_establishment_view.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+
+import '../../../testable_widget.dart';
